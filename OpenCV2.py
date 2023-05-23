967b46e2302936b9e534406ed6e65a83b7cfc61e
Cklotte27 <70385753+Cklotte27@users.noreply.github.com>
2023-05-23 00:02:24 -0500

    merging work

--- OpenCV2.py
+++ OpenCV2.py
@@ -3,8 +3,12 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
+<<<<<<< HEAD
 # Prepare data generator for standardizing frames before sending them into the model.
 data_generator = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+=======
+data_gen = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+>>>>>>> ca217e5eede55c7bb75c01b4f962b9f24f19a443
 
 # Loading the model.
 MODEL_NAME = 'CNN_model.h5'  #########################################
@@ -12,8 +16,13 @@ MODEL_NAME = 'CNN_model.h5'  #########################################
 model = load_model(MODEL_NAME)
 
 # Setting up the input image size and frame crop size.
+<<<<<<< HEAD
 IMAGE_SIZE = 50
 CROP_SIZE = 300
+=======
+INPUT_SIZE = 50
+FRAME_SIZE = 300
+>>>>>>> ca217e5eede55c7bb75c01b4f962b9f24f19a443
 
 # Creating list of available classes stored in classes.txt.
 classes_file = open("classes.txt")
@@ -22,6 +31,7 @@ classes = classes_string.split()
 classes.sort()  # The predict function sends out output in sorted order.
 
 # Preparing cv2 for webcam feed
+<<<<<<< HEAD
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -41,6 +51,26 @@ while(True):
     # Predicting the frame.
     prediction = np.array(model.predict(frame_for_model))
     predicted_class = classes[prediction.argmax()]      # Selecting the max confidence index.
+=======
+capture = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame.
+    ret, frame = capture.read()
+
+    # Target area where the hand gestures should be.
+    cv2.rectangle(frame, (0, 0), (FRAME_SIZE, FRAME_SIZE), (0, 255, 0), 3)
+    
+    cropped_image = frame[0:FRAME_SIZE, 0:FRAME_SIZE]
+    resized_frame = cv2.resize(cropped_image, (INPUT_SIZE, INPUT_SIZE))
+    reshaped_frame = (np.array(resized_frame)).reshape((1, INPUT_SIZE, INPUT_SIZE, 3)) #CNN
+    #reshaped_frame = (np.array(resized_frame)).reshape((1, 1, INPUT_SIZE, INPUT_SIZE, 3)) #Add for LSTM
+    frame_for_model = data_gen.standardize(np.float64(reshaped_frame))
+
+    # Predicting the frame.
+    prediction = np.array(model.predict(frame_for_model))
+    predicted_class = classes[prediction.argmax()]   
+>>>>>>> ca217e5eede55c7bb75c01b4f962b9f24f19a443
 
     # Preparing output based on the model's confidence.
     prediction_probability = prediction[0, prediction.argmax()]
@@ -56,14 +86,24 @@ while(True):
         # No confidence.
         cv2.putText(frame, classes[-2], (10, 450), 1, 2, (255, 255, 0), 2, cv2.LINE_AA)
 
+<<<<<<< HEAD
     # Display the image with prediction.
     cv2.imshow('frame', frame)
 
     # Press q to quit
+=======
+    
+    cv2.imshow('frame', frame)
+
+>>>>>>> ca217e5eede55c7bb75c01b4f962b9f24f19a443
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
         break
 
+<<<<<<< HEAD
 # When everything done, release the capture.
 cap.release()
+=======
+capture.release()
+>>>>>>> ca217e5eede55c7bb75c01b4f962b9f24f19a443
 cv2.destroyAllWindows()
---
+++ objectDet/RealTimeObjectDetection/labelCreater.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+import time
+import uuid
+
+IMG_PATH = 'Tensorflow/workspace/images/collectedimages'
+
+labels = ['hello', 'thankyou', 'yes', 'no', 'iloveyou']
+number_imgs = 15
+
+for label in labels:
+    !mkdir {'Tensorflow\workspace\images\collectedimages\\'+label}
+    cap = cv2.VideoCapture(0)
+    print(f'Collecting images for {label}')
+    time.sleep(5)
+    for img_num in range(number_imgs):
+        _, frame = cap.read()
+        image_name = os.path.join(IMG_PATH, label, label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(image_name, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(2)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
