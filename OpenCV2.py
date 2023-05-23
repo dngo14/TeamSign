613252168679bc4dea5032e2bac636d887b414d7
danieln3781@gmail.com <danielngo@influencewm.com>
2023-05-22 23:14:36 -0500

    formatting

--- OpenCV2.py
+++ OpenCV2.py
@@ -3,8 +3,7 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
-# Prepare data generator for standardizing frames before sending them into the model.
-data_generator = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
+data_gen = ImageDataGenerator(samplewise_center=True, samplewise_std_normalization=True)
 
 # Loading the model.
 MODEL_NAME = 'CNN_model.h5'  #########################################
@@ -12,8 +11,8 @@ MODEL_NAME = 'CNN_model.h5'  #########################################
 model = load_model(MODEL_NAME)
 
 # Setting up the input image size and frame crop size.
-IMAGE_SIZE = 50
-CROP_SIZE = 300
+INPUT_SIZE = 50
+FRAME_SIZE = 300
 
 # Creating list of available classes stored in classes.txt.
 classes_file = open("classes.txt")
@@ -22,25 +21,24 @@ classes = classes_string.split()
 classes.sort()  # The predict function sends out output in sorted order.
 
 # Preparing cv2 for webcam feed
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame.
-    ret, frame = cap.read()
+    ret, frame = capture.read()
 
     # Target area where the hand gestures should be.
-    cv2.rectangle(frame, (0, 0), (CROP_SIZE, CROP_SIZE), (0, 255, 0), 3)
+    cv2.rectangle(frame, (0, 0), (FRAME_SIZE, FRAME_SIZE), (0, 255, 0), 3)
     
-    # Preprocessing the frame before input to the model.
-    cropped_image = frame[0:CROP_SIZE, 0:CROP_SIZE]
-    resized_frame = cv2.resize(cropped_image, (IMAGE_SIZE, IMAGE_SIZE))
-    reshaped_frame = (np.array(resized_frame)).reshape((1, IMAGE_SIZE, IMAGE_SIZE, 3)) #CNN
-    #reshaped_frame = (np.array(resized_frame)).reshape((1, 1, IMAGE_SIZE, IMAGE_SIZE, 3)) #LSTM
-    frame_for_model = data_generator.standardize(np.float64(reshaped_frame))
+    cropped_image = frame[0:FRAME_SIZE, 0:FRAME_SIZE]
+    resized_frame = cv2.resize(cropped_image, (INPUT_SIZE, INPUT_SIZE))
+    reshaped_frame = (np.array(resized_frame)).reshape((1, INPUT_SIZE, INPUT_SIZE, 3)) #CNN
+    #reshaped_frame = (np.array(resized_frame)).reshape((1, 1, INPUT_SIZE, INPUT_SIZE, 3)) #Add for LSTM
+    frame_for_model = data_gen.standardize(np.float64(reshaped_frame))
 
     # Predicting the frame.
     prediction = np.array(model.predict(frame_for_model))
-    predicted_class = classes[prediction.argmax()]      # Selecting the max confidence index.
+    predicted_class = classes[prediction.argmax()]   
 
     # Preparing output based on the model's confidence.
     prediction_probability = prediction[0, prediction.argmax()]
@@ -56,14 +54,12 @@ while(True):
         # No confidence.
         cv2.putText(frame, classes[-2], (10, 450), 1, 2, (255, 255, 0), 2, cv2.LINE_AA)
 
-    # Display the image with prediction.
+    
     cv2.imshow('frame', frame)
 
-    # Press q to quit
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
         break
 
-# When everything done, release the capture.
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
